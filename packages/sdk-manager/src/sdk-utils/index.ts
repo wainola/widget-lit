1e0baa9e7f74b005b08d7da12eb322a58b2c3a41
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2023-09-08 10:49:13 -0300

    main utility functions to create asset transfers

---
+++ packages/sdk-manager/src/sdk-utils/index.ts
@@ -0,0 +1,15 @@
+import { EVMAssetTransfer, Environment, SubstrateAssetTransfer } from "@buildwithsygma/sygma-sdk-core";
+
+const createEvmAssetTransfer = async (evmWallet: EvmWallet): Promise<EVMAssetTransfer> => {
+  const assetTransfer = new EVMAssetTransfer();
+  await assetTransfer.init(evmWallet!.currentProvider!, Environment.TESTNET);
+  return assetTransfer;
+}
+
+const createSubstrateAssetTransfer = async (substrateWallet: SubstrateWallet): Promise<SubstrateAssetTransfer> => {
+  const assetTransfer = new SubstrateAssetTransfer();
+  await assetTransfer.init(substrateWallet!.currentApi!, Environment.TESTNET);
+  return assetTransfer;
+}
+
+export { createEvmAssetTransfer, createSubstrateAssetTransfer }
