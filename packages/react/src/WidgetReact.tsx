4da49e9c7ac551a5c5fd2071b89651bf8823a64e
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2023-09-14 09:38:30 -0300

    chore: updates

--- packages/react/src/WidgetReact.tsx
+++ packages/react/src/WidgetReact.tsx
@@ -2,6 +2,8 @@ import { Widget } from '@wainola/lit-widget';
 import { createComponent } from '@lit-labs/react';
 import React from 'react';
 
+console.log('react component');
+
 const WidgetReact = createComponent({
   tagName: 'widget-test',
   elementClass: Widget,
