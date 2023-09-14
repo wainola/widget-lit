7ece16dd40e4bdf042b9ea0d7c0cde5300529443
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2023-09-14 13:20:16 -0300

    chore: pipelines to check build of artifacts

--- packages/react/src/WidgetReact.tsx
+++ packages/react/src/WidgetReact.tsx
@@ -2,8 +2,6 @@ import { Widget } from '@wainola/lit-widget';
 import { createComponent } from '@lit-labs/react';
 import React from 'react';
 
-console.log('react component');
-
 const WidgetReact = createComponent({
   tagName: 'widget-test',
   elementClass: Widget,
